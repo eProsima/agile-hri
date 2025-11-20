e50622a1390aae3f898016d6acf9ba6ef53b4ca1
Carlos Ferreira González <carlosferreira@eprosima.com>
2025-11-20 08:04:40 +0100

    Add HRI_EMOTION_DETECT (#5)
    * Refs #23651: Add HRI_EMOTION_DETECT
    
    Signed-off-by: cferreiragonz <carlosferreira@eprosima.com>
    
    * Refs #23651: Delete unused VERSION file
    
    Signed-off-by: Carlos Ferreira González <carlosferreira@eprosima.com>
    
    * Refs #23651: Apply Revision
    
    Signed-off-by: Carlos Ferreira González <carlosferreira@eprosima.com>
    
    ---------
    
    Signed-off-by: cferreiragonz <carlosferreira@eprosima.com>
    Signed-off-by: Carlos Ferreira González <carlosferreira@eprosima.com>

---
+++ hri_emotion_detect/setup.py
@@ -0,0 +1,35 @@
+import os
+from glob import glob
+from setuptools import find_packages, setup
+
+package_name = 'hri_emotion_detect'
+
+setup(
+    name=package_name,
+    version='1.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        # Include all launch files.
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+        # Include all params files.
+        (os.path.join('share', package_name, 'params'), glob(os.path.join('params', '*.yaml'))),
+        # Include all models files.
+        (os.path.join('share', package_name, 'models'), glob(os.path.join('models', '*.onnx'))),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    author='Carlos Ferreira',
+    author_email='carlosferreira@eprosima.com',
+    maintainer='Carlos Ferreira',
+    maintainer_email='carlosferreira@eprosima.com',
+    description='ROS 2 node implementing emotion recognition.',
+    license='Apache License, Version 2.0',
+    entry_points={
+        'console_scripts': [
+            'node_emotion_detect = hri_emotion_detect.node_emotion_detect:main',
+        ],
+    },
+)
